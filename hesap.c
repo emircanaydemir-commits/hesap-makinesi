1584b875f90580aca4deeffcd70aebc63e16138a
emircanaydemir-commits <aydemiremircan79@gmail.com>
2026-07-13 01:45:40 +0300

    İlk sürüm: WASM Hesap Makinesi tamamlandı

---
+++ hesap.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+// yiğit yapısı
+#define max 15
+char yigitoperator[max];
+double yigitsayi[max + 1];
+int topoperator = -1;
+int topsayi = -1;
+/*----------operatör yiğiti----------*/
+// dolumu kontrol
+int dolumu_operator()
+{
+    if (topoperator == max - 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+// boşmu kontrol
+int bosmu_operator()
+{
+    if (topoperator == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
+// yiğita ekleme
+void ekle_operator(char veri)
+{
+    if (dolumu_operator())
+    {
+        return;
+    }
+    topoperator++;
+    yigitoperator[topoperator] = veri;
+}
+// yiğitan çıkarma
+char cikar_operator()
+{
+    if (bosmu_operator())
+    {
+        return '\0';
+    }
+    char cikan = yigitoperator[topoperator];
+    topoperator--;
+    return cikan;
+}
+/*--------sayi yiğiti------*/
+// boşmu kontrol
+int bosmu_sayi()
+{
+    if (topsayi == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
+// dolumu kontrol
+int dolumu_sayi()
+{
+    if (topsayi == max - 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+// ekleme
+void ekle_sayi(double veri)
+{
+    if (dolumu_sayi())
+    {
+        return;
+    }
+    topsayi++;
+    yigitsayi[topsayi] = veri;
+}
+double cikar_sayi()
+{
+    if (bosmu_sayi())
+    {
+        printf("\nyiğit bos!\n");
+        return NAN;
+    }
+    else
+    {
+        double cikan = yigitsayi[topsayi];
+        topsayi--;
+        return cikan;
+    }
+}
+// test
+void yazdirsayi()
+{
+    for (int i = 0; i <= topsayi; i++)
+    {
+        printf("%lf ", yigitsayi[i]);
+    }
+    printf("\n");
+}
+void yazdiroperator()
+{
+    for (int i = 0; i <= topoperator; i++)
+    {
+        printf("%c", yigitoperator[i]);
+    }
+    printf("\n");
+}
+// işlem önceliği kontrolu
+int kontrol(char operator)
+{
+    if (operator == '*' || operator == '%')
+    {
+        return 2;
+    }
+    else if (operator == '+' || operator == '-')
+    {
+        return 1;
+    }
+    return 0;
+}
+// işlemi kontrol edip yığıta ekle
+void hesapla_ekle()
+{
+    char islem = cikar_operator();
+    double sag = cikar_sayi();
+    double sol = cikar_sayi();
+    double sonuc = 0;
+    if (islem == '*')
+    {
+        sonuc = sol * sag;
+    }
+    else if (islem == '%')
+    {
+        if (sag == 0)
+        {
+            printf("hatalı işlem girildi!");
+            sonuc = NAN;
+        }
+        else
+        {
+            sonuc = sol / sag;
+        }
+    }
+
+    else if (islem == '+')
+    {
+        sonuc = sol + sag;
+    }
+    else if (islem == '-')
+    {
+        sonuc = sol - sag;
+    }
+    ekle_sayi(sonuc);
+}
+//parçala ve yığına ekle 
+double parcala(char ifade[]) {
+    char *gezici = ifade;
+    char *kalan;
+   while(*gezici!='\0'){
+    //eğer sayiysa direkt  yiğta ekle
+    if (isdigit(*gezici))
+    {
+        double sayi=strtod(gezici,&kalan);
+        ekle_sayi(sayi);
+        gezici=kalan;
+    }
+    //eğer " ( " görürsen direkt yiğita ekle
+    else if (gezici[0]=='(')
+    {
+        ekle_operator(gezici[0]);
+        gezici++;
+    }
+    // " ) " görürsen yiğita ekleme ondan öceki işlemleri yap en son geziciyi artır
+    else if (gezici[0]==')')
+    {
+       while (yigitoperator[topoperator]!='(')
+       {
+        hesapla_ekle();
+       }
+       cikar_operator();
+       gezici++;
+    }
+    //işlem önceliğine göre hesapla
+    // bir önceki işlem şuan girenden güçlüyse işlemi eklemeden önce hesapla
+    else if(gezici[0]=='+' || gezici[0]=='-' || gezici[0]=='*' || gezici[0]=='%'){
+        while (!bosmu_operator()&&kontrol(yigitoperator[topoperator])>=kontrol(gezici[0]))
+        {
+            hesapla_ekle();
+        }
+        ekle_operator(gezici[0]);
+        gezici++;
+        
+    }
+    //başka ifade varsa atla
+    else{
+        gezici++;
+    }
+   }
+   //kalan ifadelerin tümünü hesapla
+    while (!bosmu_operator())
+    {
+       hesapla_ekle();
+    }
+    return yigitsayi[topsayi];
+    
+}
+
+int main()
+{
+    char ifade[] = "25-(32*5+1)%3"; // x yerine * yazdık
+    double sonuc = parcala(ifade);
+    printf("Sonuc: %lf\n", sonuc);
+    return 0;
+}
---
+++ script.js
@@ -0,0 +1,116 @@
+//arka plan animasyonu
+const arka = document.getElementById("arkaplan");
+for (let i = 0; i < 60; i++) {
+    const yeni = document.createElement("div");
+    yeni.classList.add("sayilar");
+    let rastgele;
+    rastgele = Math.floor(Math.random() * 2);
+    yeni.textContent = rastgele;
+    rastgele = Math.floor(Math.random() * 100);
+    yeni.style.left = rastgele + "vw";
+    rastgele = Math.random() * 10;
+    yeni.style.animationDelay = -rastgele + "s";
+    rastgele = Math.floor(Math.random() * 30 + 10);
+    yeni.style.fontSize = rastgele + "px";
+    arka.appendChild(yeni);
+}
+//tıkladığında yaz
+const yazılacak = document.getElementById("yazılacak");
+const bir = document.getElementById("bir");
+bir.addEventListener("click", function () {
+    yazılacak.textContent += "1";
+});
+const iki = document.getElementById("iki");
+iki.addEventListener("click", function () {
+    yazılacak.textContent += "2";
+});
+const uc = document.getElementById("uc");
+uc.addEventListener("click", function () {
+    yazılacak.textContent += "3";
+})
+const dort = document.getElementById("dort");
+dort.addEventListener("click", function () {
+    yazılacak.textContent += "4";
+});
+const bes = document.getElementById("bes");
+bes.addEventListener("click", function () {
+    yazılacak.textContent += "5";
+})
+const altı = document.getElementById("altı");
+altı.addEventListener("click", function () {
+    yazılacak.textContent += "6";
+})
+const yedi = document.getElementById("yedi");
+yedi.addEventListener("click", function () {
+    yazılacak.textContent += "7";
+})
+const sekiz = document.getElementById("sekiz");
+sekiz.addEventListener("click", function () {
+    yazılacak.textContent += "8";
+});
+const dokuz = document.getElementById("dokuz");
+dokuz.addEventListener("click", function () {
+    yazılacak.textContent += "9";
+});
+const sifir = document.getElementById("sifir");
+sifir.addEventListener("click", function () {
+    yazılacak.textContent += "0";
+});
+const solayrac = document.getElementById("ayracsol");
+solayrac.addEventListener("click", function () {
+    yazılacak.textContent += "(";
+});
+const sagayrac = document.getElementById("ayracsag");
+sagayrac.addEventListener("click", function () {
+    yazılacak.textContent += ")";
+});
+const nokta = document.getElementById("nokta");
+nokta.addEventListener("click", function () {
+    yazılacak.textContent += ".";
+})
+const bol = document.getElementById("bol");
+bol.addEventListener("click", function () {
+    yazılacak.textContent += "÷";
+});
+const topla = document.getElementById("artı");
+topla.addEventListener("click", function () {
+    yazılacak.textContent += "+";
+});
+const cıkar = document.getElementById("eksi");
+cıkar.addEventListener("click", function () {
+    yazılacak.textContent += "-";
+});
+const carp = document.getElementById("carp");
+carp.addEventListener("click", function () {
+    yazılacak.textContent += "x";
+});
+//temizleme butonu
+const temizle = document.getElementById("temizle");
+const sonuc = document.getElementById("sonuc");
+temizle.addEventListener("click", function () {
+    yazılacak.textContent = "";
+    sonuc.textContent = "";
+});
+//eşittir butonu ve c kodunu bağlama
+const esittir = document.getElementById("esittir");
+esittir.addEventListener("click",function(){
+    let ifade=yazılacak.textContent;
+    //metindeki bazı ifadeleri c diline uygun hale getirelim
+    ifade=ifade.replace(/x/g,"*");
+    ifade=ifade.replace(/÷/g,"%");
+    console.log(ifade);
+    // c deki parçala fonksiyonunu çağır
+    try{
+        let sonucdeger=Module.ccall(
+            'parcala',
+            'number',
+            ['string'],
+            [ifade]
+
+        );
+        sonuc.textContent=sonucdeger;
+    }
+    catch(error){
+        sonuc.textContent="Hata!";
+    }
+})
